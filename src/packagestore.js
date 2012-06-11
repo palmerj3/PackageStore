a7d51e0ec5e6d9ae7caa3ffbb9977eb23bd43855
Jason Palmer <jpalmer@spotify.com>
2012-06-10 22:13:28 -0400

    Removed the init function along with the several unnecessary private variables.  Storage Key and encrypt/decrypt functions are now passed directly to the constructor.

--- src/packagestore.js
+++ src/packagestore.js
@@ -1,10 +1,26 @@
-var PackageStore = function () {
+var PackageStore = function (args) {
   'use strict';
+  var _encrypt = function (str) { return str; },
+    _decrypt = function (str) { return str },
+    _storageKey = 'PackageStore';
+  
+  // Initialize params
+  if (typeof (args) !== 'undefined') {
+    if (typeof (args['storageKey']) !== 'undefined') {
+      _storageKey = args['storageKey'];
+    }
 
-  var _storageKey = null,
-    _encrypt = null,
-    _decrypt = null,
-    _store = function (key, value) {
+    if (typeof (args['encrypt']) === 'function') {
+      _encrypt = args['encrypt'];
+    }
+
+    if (typeof (args['decrypt']) === 'function') {
+      _decrypt = args['decrypt'];
+    }
+  }
+
+  return {
+    setItem : function (key, value) {
       var storage_contents = {};
       if (localStorage.getItem(_storageKey)) {
         storage_contents = JSON.parse(_decrypt(localStorage.getItem(_storageKey)));
@@ -12,44 +28,21 @@ var PackageStore = function () {
       storage_contents[key] = value;
       return localStorage.setItem(_storageKey, _encrypt(JSON.stringify(storage_contents)));
     },
-    _retrieve = function (key) {
+
+    getItem : function (key) {
       var storage_contents = {};
       if (localStorage.getItem(_storageKey)) {
         storage_contents = JSON.parse(_decrypt(localStorage.getItem(_storageKey)));
       }
       return typeof (storage_contents[key]) === 'undefined' ? '' : storage_contents[key];
     },
-    _remove = function (key) {
+
+    removeItem : function (key) {
       if (localStorage.getItem(_storageKey)) {
         var storage_contents = JSON.parse(_decrypt(localStorage.getItem(_storageKey)));
         delete(storage_contents[key]);
         return localStorage.setItem(_storageKey, _encrypt(JSON.stringify(storage_contents)));
       }
     }
-
-  return {
-    settings : {
-      storageKey : 'I_Highly_Recommend_Changing_This',
-      encrypt : function (str) { return str;  },
-      decrypt : function (str) { return str; }
-    },
-
-    setItem : function (key, value) {
-      return _store(key, value);
-    },
-
-    getItem : function (key) {
-      return _retrieve(key);
-    },
-    
-    removeItem : function (key) {
-      return _remove(key);
-    },
-
-    init : function () {
-      _storageKey = this.settings.storageKey;
-      _encrypt = this.settings.encrypt;
-      _decrypt = this.settings.decrypt;
-    }
-  };
+  }
 };

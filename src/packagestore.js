772c9aacccb9095caca5bd2879fd0006e06cd845
Jason Palmer <jpalmer@spotify.com>
2012-12-01 15:55:24 -0500

    Updated design pattern so it is more explicit about what are public and private methods

--- src/packagestore.js
+++ src/packagestore.js
@@ -26,51 +26,49 @@ var PackageStore = function (args) {
     }
   }
 
-  return {
-    setItem : function (key, value) {
-      var storage_contents = _getContents();
-      storage_contents[key] = value;
-      return localStorage.setItem(_storageKey, _encrypt(JSON.stringify(storage_contents)));
-    },
+  this.setItem = function (key, value) {
+    var storage_contents = _getContents();
+    storage_contents[key] = value;
+    return localStorage.setItem(_storageKey, _encrypt(JSON.stringify(storage_contents)));
+  };
 
-    getItem : function (key) {
-      var storage_contents = _getContents();
-      return typeof (storage_contents[key]) === 'undefined' ? null : storage_contents[key];
-    },
+  this.getItem = function (key) {
+    var storage_contents = _getContents();
+    return typeof (storage_contents[key]) === 'undefined' ? null : storage_contents[key];
+  };
 
-    removeItem : function (key) {
-      var storage_contents = _getContents();
-      delete(storage_contents[key]);
-      return localStorage.setItem(_storageKey, _encrypt(JSON.stringify(storage_contents)));
-    },
-    
-    clear : function () {
-      return localStorage.removeItem(_storageKey);
-    },
-    
-    length : function () {
-      var storage_contents = _getContents(),
-        cnt = 0;
-      for (var key in storage_contents) {
-        if (storage_contents.hasOwnProperty(key)) {
-          cnt += 1;
-        }
+  this.removeItem = function (key) {
+    var storage_contents = _getContents();
+    delete(storage_contents[key]);
+    return localStorage.setItem(_storageKey, _encrypt(JSON.stringify(storage_contents)));
+  };
+  
+  this.clear = function () {
+    return localStorage.removeItem(_storageKey);
+  };
+  
+  this.length = function () {
+    var storage_contents = _getContents(),
+      cnt = 0;
+    for (var key in storage_contents) {
+      if (storage_contents.hasOwnProperty(key)) {
+        cnt += 1;
       }
-      return cnt;
-    },
-    
-    key : function (i) {
-      var storage_contents = _getContents(),
-        cnt = 0;
-      for (var key in storage_contents) {
-        if (storage_contents.hasOwnProperty(key)) {
-          if (cnt === i) {
-            return key;
-          }
-          cnt += 1;
-        }
-      }
-      return null;
     }
-  }
+    return cnt;
+  };
+  
+  this.key = function (i) {
+    var storage_contents = _getContents(),
+      cnt = 0;
+    for (var key in storage_contents) {
+      if (storage_contents.hasOwnProperty(key)) {
+        if (cnt === i) {
+          return key;
+        }
+        cnt += 1;
+      }
+    }
+    return null;
+  };
 };
